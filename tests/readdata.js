04ad60e99500b41916f2dfe1e5b18159f87fe650
Ayaka <user.satoshi69@gmail.com>
2022-04-16 16:21:44 +0530

    updated readdata.js for widecolumn

--- tests/readdata.js
+++ tests/readdata.js
@@ -14,7 +14,7 @@ console.log("WideColumn")
 
 fs.readdir("./columndatabase/main/").then((d) => {
   d.forEach((f) => {
-    fs.readFile(`./database/main/${f}`).then((b) => {
+    fs.readFile(`./columndatabase/main/${f}`).then((b) => {
       console.log(`${f} -> ${b.byteLength} bytes`);
     });
   });
